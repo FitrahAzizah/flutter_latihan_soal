c4c5cc268118741a34ddf2e0af6c046e2155640e
fitr037 <fitr037@brin.go.id>
2022-10-03 15:00:24 +0700

    menambahkan login_page.dart

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../constants/r.dart';
+
 class loginPage extends StatefulWidget {
   const loginPage({super.key});
   static const String route = "login_screen";
@@ -14,7 +16,118 @@ class _loginPageState extends State<loginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+        backgroundColor: R.colors.grey,
+        body: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(R.strings.login,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    )),
+              ),
+              SizedBox(height: 20),
+              Image.asset(R.assets.imageLogin),
+              SizedBox(height: 35),
+              Text(
+                R.strings.welcome,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                R.strings.loginDescription,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: R.colors.greySubtitle,
+                ),
+              ),
+              Spacer(),
+              ButtonLogin(
+                backgroundColor: Colors.white,
+                borderColor: R.colors.primary,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(R.assets.icGoogle),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      R.strings.loginWithGoogle,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500,
+                        color: R.colors.blackLogin,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ButtonLogin(
+                backgroundColor: Colors.black,
+                borderColor: Colors.black,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(R.assets.icApple),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      R.strings.loginWithApple,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class ButtonLogin extends StatelessWidget {
+  const ButtonLogin({
+    Key? key,
+    required this.backgroundColor,
+    required this.child,
+    required this.borderColor,
+  }) : super(key: key);
+
+  final Color backgroundColor;
+  final Widget child;
+  final Color borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 12),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+            side: BorderSide(
+              color: borderColor,
+            ),
+          ),
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          fixedSize: Size(MediaQuery.of(context).size.width * 0.8, 50),
+        ),
+        onPressed: () {},
+        child: child,
+      ),
     );
   }
 }
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -6,6 +6,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:latihan_soal/view/login_page.dart';
 
+import '../constants/r.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
   static const String route = "splash_screen";
@@ -16,9 +18,9 @@ class SplashScreen extends StatelessWidget {
       Navigator.of(context).pushNamed(loginPage.route);
     });
     return Scaffold(
-      backgroundColor: Color(0xff01bdc2),
+      backgroundColor: R.colors.primary,
       body: Center(
-        child: Image.asset("assets/auth/ic_splash.png"),
+        child: Image.asset(R.assets.icSplash),
       ),
     );
   }
